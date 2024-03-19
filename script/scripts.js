7bda12ae1be9759bf7099e703c4c18d8574322b9
Ignacio Moreira <iamoreira642@gmail.com>
2024-03-18 21:34:41 -0300

    cambios en funcion de creacion de parrafos, mejora estetica, nuevo div

--- script/scripts.js
+++ script/scripts.js
@@ -1,7 +1,8 @@
 
 let mazoCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const formDiv = document.getElementById('formDiv');
-const gameContainer = document.getElementById('gameContainer');
+const gameContainer = document.querySelector('.gameContainer');
+const cardContainer = document.querySelector('.cardContainer');
 
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -28,40 +29,17 @@ function juego() {
     let jugador = localStorage.getItem('jugador');
     const welcomeTitle = document.getElementById('welcome');
     welcomeTitle.innerText = `Comencemos a jugar, ${jugador}.`;
-    const cartas = document.querySelectorAll('#gameContainer p');
 
-    console.log(cartas);
-    /* alert(`Tu primera carta tiene el número ${cartaElegida}`);
-    let pedirOtra = confirm(`¿Quieres otra carta?`);
-    if (pedirOtra == false) {
-        alert(`Fin del juego. Hasta la próxima.`);
-    } else {
-        let nuevaCarta = mazo[Math.floor(Math.random() * mazo.length)];
-        let sumaCartas = cartaElegida + nuevaCarta;
-        while (sumaCartas <= 21 || pedirOtra == true){
-            alert(`Tus cartas suman ${sumaCartas}`)
-            if (sumaCartas == 21) {
-                alert('¡Blackjack!');
-                localStorage.clear();
-                break;
-            } else if (pedirOtra == false) {
-                alert('Hasta pronto.');
-                localStorage.clear();
-                break;
-            } else if(sumaCartas > 21) {
-                alert("¡Perdiste! Fin del juego.")
-                
-                break
-            };
-            pedirOtra = confirm('¿Quieres otra carta?');
-            nuevaCarta = mazo[Math.floor(Math.random() * mazo.length)];
-            sumaCartas = sumaCartas + nuevaCarta;
-        }
-    } */
+    const hitBtn = document.getElementById('hit');
+    hitBtn.addEventListener('click', (click) =>{
+        let muestraCarta = document.createElement('p');
+        muestraCarta.innerText = `${cartaElegida}`;
+        cardContainer.appendChild(muestraCarta);
+        cartaElegida = mazo[Math.floor(Math.random() * mazo.length)];
+    });
+
+    const stayBtn = document.getElementById('stay');
+    stayBtn.addEventListener('click', (click) => {
+        
+    });
 };
-/* 
-if (comienzo == true) {
-    juego();
-} else {
-    alert("¡Hasta luego!")
-} */
